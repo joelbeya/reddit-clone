18d437ef2425236f77be2280d26dd3ef44ec10eb
joelbeya <joelbeya.bj@gmail.com>
2021-01-30 00:12:12 +0100

    Security package added correctly and SecurityConfig updated

--- back-end/src/main/java/com/redditclone/backend/BackEndApplication.java
+++ back-end/src/main/java/com/redditclone/backend/BackEndApplication.java
@@ -1,13 +1,18 @@
 package com.redditclone.backend;
 
+import com.redditclone.backend.configuration.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@Import(SwaggerConfiguration.class)
 @SpringBootApplication
 public class BackEndApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackEndApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndApplication.class, args);
+    }
 
 }
