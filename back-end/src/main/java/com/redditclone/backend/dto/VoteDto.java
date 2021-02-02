72d9878c26d60e41f1fc422d3ab64db8abd65a45
joelbeya <joelbeya.bj@gmail.com>
2021-02-03 00:21:24 +0100

    Service package added correctly

---
+++ back-end/src/main/java/com/redditclone/backend/dto/VoteDto.java
@@ -0,0 +1,14 @@
+package com.redditclone.backend.dto;
+
+import com.redditclone.backend.model.VoteType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VoteDto {
+    private VoteType voteType;
+    private Long postId;
+}
